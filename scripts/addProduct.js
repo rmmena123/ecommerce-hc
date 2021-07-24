b752009c4930903aef9509439de9345aa284eb19
Rodrigo Machado Mena <rm.mena86@gmail.com>
2021-07-23 23:56:15 -0300

    Landing Page, Armazenamento de E-mail e Database Produtos

---
+++ scripts/addProduct.js
@@ -0,0 +1,46 @@
+/* ---------- DATABASE ---------- */
+
+var degionDesktop = {
+	id: 1,
+	name: "Degion Desktop",
+	price: 2000,
+};
+
+var degionAllInOne = {
+	id: 2,
+	name: "Degion All In One",
+	price: 2500,
+};
+
+var degionNotebook = {
+	id: 3,
+	name: "Degion Notebook",
+	price: 3000,
+};
+
+var degionGamer = {
+	id: 4,
+	name: "Degion Gamer",
+	price: 3500,
+};
+
+// PEGA O ID
+// SWITCH COM IDS
+// ARMAZENA O OBJETO NO LOCAL STORAGE
+
+/*
+const inputEmail = document.getElementById("email");
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+	const key = "email";
+	const value = inputEmail.value;
+
+	if (key && value) {
+		localStorage.setItem(key, value);
+		alert("E-mail cadastrado com sucesso!");
+	} else {
+		alert("Campo e-mail é obrigatório!");
+	}
+});
+*/
